918cd745288cbb4750ed846cad1cc29fa9c8673e
sareh <sareh.heidari@gmail.com>
2015-12-16 12:17:19 +0000

    Added a 'process.env...' section for MongoDB database name

--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
 module.exports = {
   'secret': 'supersecretloginwithjwts',
-  'database': MONGOLAB_URI || 'mongodb://localhost:27017/expresstemplate'
+  'database': process.env.MONGOLAB_URI || 'mongodb://localhost:27017/expresstemplate'
 };
