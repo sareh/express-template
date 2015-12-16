30dd424d27a15d83ffacce1ead8f70c2bd8030ba
sareh <sareh.heidari@gmail.com>
2015-12-16 16:02:17 +0000

    Tidied up chats controller

--- controllers/chatsController.js
+++ controllers/chatsController.js
@@ -2,35 +2,43 @@ var Chat   = require('../models/chat');
 
 function chatsIndex(req, res) {
   Chat.find(function(err, chats){
-    if (err) return res.status(404).json({ message: 'Sorry, something went wrong.' });
-    res.status(200).json({ chats: chats });
+    if (err) return res.status(404).send({ message: 'Sorry, something went wrong.' });
+    res.status(200).send(chats);
   });
 }
 
 function chatsShow(req, res) {
   Chat.findById(req.params.id, function(){
-    if (err) return res.status(404).json({ message: 'Sorry, something went wrong.' });
-    res.status(200).json({ chat: chat });
+    if (err) return res.status(404).send({ message: 'Sorry, something went wrong.' });
+    res.status(200).send(chat);
   });
 }
 
-function chatsCreate(req, res){
+function chatsCreate(req, res) {
   var chat = new Chat(req.body.chat);
+  chat.participants.push(req.body.chat.created_by);
   chat.save(function(err){
     if (err) return res.status(500).send(err);
+    res.status(201).send(chat);
+  });
+}
 
-    // Pushes in chat to the User's model in the user.chats array
-    var id = req.body.chat.user_id;
-    User.findById(id, function(err, user){
-       user.chats.push(chat);
-       user.save();
-       return res.status(201).send(chat)
-    });
+function chatJoin(req, res) {
+  Chat.findById(req.params.id, function(){
+    if (err) return res.status(404).send({ message: 'Sorry, something went wrong.' });
+    
+    var joiningUserId = req.body.currentUser._id;
+    chat.participants.push(joiningUserId);
+    res.status(200).send(chat);
+    // res.status(200).json({ chat: chat ,
+    //                       message: "Welcome to the chat"
+    //                     });
   });
 }
 
 module.exports = {
   chatsIndex:  chatsIndex,
   chatsShow:   chatsShow,
-  chatsCreate: chatsCreate
+  chatsCreate: chatsCreate,
+  chatJoin:    chatJoin
 }
